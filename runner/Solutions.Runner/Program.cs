3c13fd4e84f238cbbc8f4f3263299e13d65418ea
pantherF <dev@panther.fun>
2026-07-13 16:26:56 +0200

    feat: Add proper structure to solution library; test functionality with runner console app

--- runner/Solutions.Runner/Program.cs
+++ runner/Solutions.Runner/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Solutions;
+
+Console.WriteLine(PeriodicTableWords.CanSpell("nana"));
+Console.WriteLine(LeetTranslator.Translate("string"));
+
+var primeFactors = PrimeFactorization.PrimeFactors(34);
+
+foreach (var factor in primeFactors)
+{
+    Console.WriteLine(factor);   
+}
+
+Console.WriteLine(RoundToNearestMultiple.Round(12, 5));
--- src/Solutions/FreeCodeCampDailyChallenges/PrimeFactorization.cs
+++ src/Solutions/FreeCodeCampDailyChallenges/PrimeFactorization.cs
@@ -1,76 +1,64 @@
-bool IsPrime(uint number)
-{
-    var numbers = new List<uint>();
+namespace Solutions;
 
-    for (uint i = 1; i <= number; i++)
+public static class PrimeFactorization
+{
+    private static bool IsPrime(uint number)
     {
-        if (number % i == 0)
+        var numbers = new List<uint>();
+
+        for (uint i = 1; i <= number; i++)
         {
-            numbers.Add(i);
+            if (number % i == 0)
+            {
+                numbers.Add(i);
+            }
+        }
+
+        if (numbers.Count == 2 && numbers.Contains(1) && numbers.Contains(number))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static List<uint> Primes(uint until)
+    {
+        List<uint> primes = [];
+
+        for (uint i = 1; i <= until; i++)
+        {
+            if (IsPrime(i))
+            {
+                primes.Add(i);
+            }
+        }
+
+        return [.. primes.OrderByDescending(n => n)];
+    }
+
+    private static void Factor(uint number, List<uint> primes, ref List<uint> factors)
+    {
+        foreach (var prime in primes)
+        {
+            if (number % prime == 0)
+            {
+                factors.Add(prime);
+
+                Factor(number / prime, Primes(number / prime), ref factors);
+                break;
+            }
         }
     }
 
-    if (numbers.Count == 2 && numbers.Contains(1) && numbers.Contains(number))
+    public static List<uint> PrimeFactors(uint number)
     {
-        return true;
+        var primes = Primes(20);
+        var factors = new List<uint>();
+
+        Factor(number, primes, ref factors);
+
+        return factors;
     }
 
-    return false;
-}
-
-List<uint> Primes(uint until)
-{
-    List<uint> primes = [];
-
-    for (uint i = 1; i <= until; i++)
-    {
-        if (IsPrime(i))
-        {
-            primes.Add(i);
-        }
-    }
-
-    return [.. primes.OrderByDescending(n => n)];
-}
-
-List<uint> PrimeFactors(uint number)
-{
-    var primes = Primes(20);
-    var factors = new List<uint>();
-
-    Factor(number, primes, ref factors);
-
-    return factors;
-}
-
-void Factor(uint number, List<uint> primes, ref List<uint> factors)
-{
-    foreach (var prime in primes)
-    {
-        Console.WriteLine($"-------------");
-        Console.WriteLine($"#: {number}");
-
-        Console.WriteLine($"prime: {prime}");
-
-        Console.WriteLine($"# == prime: {number == prime}");
-
-        Console.WriteLine($"# % prime: {number % prime}");
-
-        Console.WriteLine($"# / prime: {number / prime}");
-        Console.WriteLine($"-------------");
-
-        if (number % prime == 0)
-        {
-            factors.Add(prime);
-
-            Factor(number / prime, Primes(number / prime), ref factors);
-            break;
-        }
-    }
-}
-
-var factors = PrimeFactors(42);
-foreach (var factor in factors)
-{
-    Console.WriteLine(factor);
 }
