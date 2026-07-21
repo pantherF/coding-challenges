d5f1492d21a86904ffb33c5daae57b97e511b893
pantherF <dev@panther.fun>
2026-07-21 17:04:58 +0200

    feat(tests): add test for RoundToNearestMultiple method

---
+++ tests/Solutions.Tests/RoundToNearestMultipleTest.cs
@@ -0,0 +1,29 @@
+﻿using Solutions.FreeCodeCampDailyChallenges;
+
+namespace Solutions.Tests;
+
+public class RoundToNearestMultipleTest
+{
+    [Fact]
+    public void FirstGreaterThanSecond_ThrowsException()
+    {
+        uint a = 3;
+        uint b = 5;
+
+        var exception = Assert.Throws<Exception>(() => RoundToNearestMultiple.Round(a, b));
+
+        Assert.Equal("First must be greater than second.", exception.Message);
+    }
+
+    [Theory]
+    [InlineData(5, 3, 6)]
+    [InlineData(17, 4, 16)]
+    [InlineData(43, 5, 45)]
+    [InlineData(38, 11, 33)]
+    [InlineData(93, 12, 96)]
+    public void RoundToNearestMultiple_VariousInputs_ReturnsExpected(uint a, uint b, uint expected)
+    {
+        uint result = RoundToNearestMultiple.Round(a, b);
+        Assert.Equal(expected, result);
+    }
+}
